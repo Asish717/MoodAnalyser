cb24d88717a5c67781a983e40411c88545eb1068
Asish <asishkumar717@gmail.com>
2023-12-26 11:25:55 +0530

    Added: Adding mood analyser with constructor to return happy

--- src/test/java/com/java/MoodTest.java
+++ src/test/java/com/java/MoodTest.java
@@ -4,11 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodTest {
-    MoodAnalyser analyser=new MoodAnalyser("I am in Sad Mood");
+    MoodAnalyser analyser=new MoodAnalyser(" ");
     @Test
     public void test2(){
         String check=analyser.mood();
         System.out.println(check);
-        Assert.assertEquals("SAD",check);
+        Assert.assertEquals("HAPPY",check);
     }
 }
